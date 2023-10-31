86959447f4db87912f0c0475993a697eef9ee6c2
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-10-31 13:29:03 +0100

    update

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Favicon extractor
 
+### Installation
+
+source .venv/bin/activate
+
 ## To-do
 
-- extracting with right extension
+- ~~"invalid url" error~~ probably fixed
+  https://twitter.com False
+  https://tellonym.me False
+  https://codepen.io False
+  https://chinaphonearena.com
+- ~~to much time to checkUrl~~ fix
+- "No connection adapters were found for 'data:,'" error (time sleep)
---
+++ favicon_extractor_gecko.py
@@ -0,0 +1,122 @@
+import json
+import requests
+import os
+import os.path
+import re
+from bs4 import BeautifulSoup
+from termcolor import colored
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Firefox()
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+
+def write_logs(url, e):
+    with open('log.txt', 'a') as f:
+            f.write(f"{url} ==> {e}\n")
+
+def check_url(url):
+    print(colored('checkUrl', "magenta"))
+    try:
+        response = requests.head(url, headers=headers)
+        status_code = response.status_code
+        return status_code < 400
+    except requests.exceptions.RequestException as e:
+        write_logs(url, e)
+        print(f"{colored('  ==>', 'light_red')} CheckUrl error = {url} code = {colored(e, 'light_red', attrs=['underline'])}")
+        return False
+        
+
+def download(url, name):
+    print(colored('download', "magenta"))
+    try:
+        print("iconrepup")
+        print(url)
+        icon_response = requests.get(url, headers=headers)
+        print("iconrep")
+        if counter == 0:
+            name_ = name
+        else:
+            name_ = name + f"({counter})"
+        print("fileextension")
+
+        file_extension = os.path.splitext(url)[1]
+        if "?" in file_extension:
+            file_extension = re.search(r'\.(.*?)\?', url)
+        with open('extracted/' + name_ + file_extension, 'wb') as file:
+            file.write(icon_response.content)
+        print(f"{colored('  ==>', 'green')} Successful download {url}")
+    except Exception as e:
+        write_logs("Failed download error for URL for " + url, e)
+        print(f"{colored('  ==>', 'light_red')} Failed download error for URL = {url} code = {colored(e, 'light_red', attrs=['underline'])}")
+
+with open('url.json') as f:
+    data = json.load(f)
+
+for name, url in data["sites"].items():
+    try:
+        print(f"{colored('::', 'light_blue')} Start with {url}")
+
+        if check_url(url):
+
+            def getUrl(url):
+                print(colored('getUrl', "magenta"))
+                driver.get(url)
+                favicon = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'link[rel="icon"]'))
+                )
+
+                WebDriverWait(driver, 30).until(
+                    lambda driver: favicon.get_attribute('href') not in ['data:,', '']
+                )
+
+                tags = driver.find_elements(By.CSS_SELECTOR, "link[rel*='icon']")
+                return tags
+            
+            link_tags = getUrl(url)
+            print(colored('first linktagsdef', "magenta"))
+
+            print([tag.get_attribute('outerHTML') for tag in link_tags]) # to see the tags
+            
+
+            if link_tags == []:
+                print(colored('secondlinktagsdef', "magenta"))
+                print(colored("  ==> retry with search url", "red"))
+                if check_url(url + "/favicon.ico"):
+                    counter = 0
+                    download(url + "/favicon.ico", name)
+                    continue
+                else:
+                    write_logs(url, "the site does not contain a favicon, or there is a problem in the script")
+                    print(f"{colored('  ==>', 'light_red')} Error for URL = {url} code = {colored('the site does not contain a favicon, or there is a problem in the script', 'light_red', attrs=['underline'])}")
+
+            icon_types = [['icon'], ['shortcut', 'icon'], ['apple-touch-icon', 'apple-touch-icon-precomposed'], ['fluid-icon', 'mask-icon']]
+            counter = 0
+            for link_tag in link_tags:
+                print(colored('firstloop', "magenta"))
+                rels = link_tag.get_attribute('rel').split()
+                for icon_type in icon_types:
+                    print(colored('secondloop', "magenta"))
+                    if all(rel in rels for rel in icon_type):
+                        icon_url = link_tag.get_attribute('href')
+                        print(icon_url)
+                        download(icon_url, name)
+                        counter += 1
+        else:
+            write_logs(url, "invalid url")
+            print(f"{colored('  ==>', 'light_red')} Error for URL = {url} code = {colored('invalid url', 'light_red', attrs=['underline'])}")
+
+
+    except Exception as e:
+        write_logs(url, e)
+        print(f"{colored('  ==>', 'light_red')} Error for URL = {url} code = {colored(e, 'light_red', attrs=['underline'])}")
+
+print(colored('Successful download', 'green'), ' look at log.txt to see errors')
+
+driver.quit()
